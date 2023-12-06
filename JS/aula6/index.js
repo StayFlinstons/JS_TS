a6c785ec65bb8c95ce5523600cf2e67a82fa0b6f
Richard <88545769+StayFlinstons@users.noreply.github.com>
2023-12-06 15:11:03 -0300

    Aulas

---
+++ JS/aula6/index.js
@@ -0,0 +1,12 @@
+let nome;
+nome = 'João';
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
+
+// Não podemos criar variáveis com palavras reservadas, Ex let if;
+// Variáveis precisam ter nomes significativos, Ex: let nome;
+// Nome pode comçar variável com número, espaços ou traços;
+// Utilizamos camlCase, Ex: let nomeCompleto;
+// Não podemos redeclarar variáveis com let;
+// NÃO UTILIZE VAR, UTILIZE LET.
---
+++ JS/aula7/index.js
@@ -0,0 +1,10 @@
+// const nome = 'João';
+// console.log(nome);
+
+const nome = 'Teste';
+const primeiroNumero = 10; // Number
+const segundoNumero = 20;
+let conta = primeiroNumero * segundoNumero;
+conta = conta * 10;
+console.log( typeof nome, typeof conta);
+
