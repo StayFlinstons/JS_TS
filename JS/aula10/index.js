cb9e683e0b6fa2ae4187375089be52d502cbd9e6
Richard <88545769+StayFlinstons@users.noreply.github.com>
2023-12-12 15:44:57 -0300

    1

---
+++ JS/aula10/index.js
@@ -0,0 +1,7 @@
+const nome = 'Ismael'; // string.
+const num = 10; // number.
+let nome1; // undefined = valor nulo.
+let nome2 = null; // nulo, não são iguais. 
+const boolean = true; // true ou false (lógico).
+
+console.log(typeof nome, nome); // ver tipo.
