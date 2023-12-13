24cb64c14e259b8f650efb00ee04365eccd856ae
Richard <88545769+StayFlinstons@users.noreply.github.com>
2023-12-12 23:57:55 -0300

    Aulas

---
+++ JS/aula10/index2.js
@@ -0,0 +1,28 @@
+/*
+
+Aritméticos (+, -, /, *, ** e %)
+
+*/
+
+const num1 = 8;
+const num2 = 4;
+console.log('Resultado:', num1 % num2);
+
+let contador = 1;
+contador++;
+contador++;
+contador++;
+console.log('Contador:', contador)
+
+let contador2 = 1;
+contador2--;
+contador2--;
+contador2--;
+console.log('Contador2:', contador2);
+
+const passo = 3;
+let contador3 = 0;
+contador3 += passo;
+contador3 *= passo;
+contador3 /= passo;
+console.log('Contador3', contador3);
