68c3cf51415d1e77e73820a240f2b496329adc17
Richard <88545769+StayFlinstons@users.noreply.github.com>
2024-04-17 12:00:22 -0300

    Update index.js

--- JS/aula15/index.js
+++ JS/aula15/index.js
@@ -1,6 +1,9 @@
-let num1 = 9.49;
+// let num1 = 9.49;
 // num1 = Math.floor(num1);
 // num1 = Math.ceil(num1);
 // num1 = Math.round(num1);
 
-console.log(num1);
+// console.log(num1);
+
+let aleatorio = Math.round(Math.random() * 10) + 1;
+console.log(aleatorio);
