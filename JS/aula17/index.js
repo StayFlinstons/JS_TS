32ebc885df84e20943dc141b272db439fd673200
Richard <88545769+StayFlinstons@users.noreply.github.com>
2024-05-08 14:28:01 -0300

    funcoes

---
+++ JS/aula17/index.js
@@ -0,0 +1,12 @@
+// function teste(x, y){
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// const resultado = teste(2, 2);
+// console.log(resultado);
+
+const raiz = n =>  n ** 0.5;
+
+console.log(raiz(25));
+console.log(raiz(9));
---
+++ JS/positivook/index.js
@@ -0,0 +1,19 @@
+let aleatorio = Math.round(Math.random() * 4) + 1;
+
+console.log(aleatorio)
+
+if (aleatorio == 1){
+    console.log('Perfeito')
+}
+else if (aleatorio == 2){
+    console.log('Ok')
+}
+else if (aleatorio == 3){
+    console.log('Mesmo')
+}
+else if (aleatorio == 4){
+    console.log('Vets')
+}
+else if (aleatorio == 5){
+    console.log('Lemaozin')
+}
