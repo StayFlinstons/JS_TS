61c583e57af3d2f39ad278f0810e689af43da26c
Richard <88545769+StayFlinstons@users.noreply.github.com>
2025-05-13 10:20:08 -0300

    uma bosta

---
+++ JS/aula47/assets/js/main.js
@@ -0,0 +1,16 @@
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.inciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+iniciar.addEventListener('click', function(event){
+
+});
+
+pausar.addEventListener('click', function(event){
+    
+});
+
+zerar.addEventListener('click', function(event){
+    
+});
