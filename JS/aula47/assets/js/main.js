435d0d99766409b0d9f86ad83d149dff3986a8cf
Richard <88545769+StayFlinstons@users.noreply.github.com>
2025-05-15 10:21:49 -0300

    holy moly

--- JS/aula47/assets/js/main.js
+++ JS/aula47/assets/js/main.js
@@ -1,16 +1,47 @@
+function relogio(){
+    function ciraHoraDosSegundos(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.inciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
 
-iniciar.addEventListener('click', function(event){
+function iniciaRelogio(){
+    timer = setInterval (function() {
+        segundos++;
+        relogio.innerHTML = ciraHoraDosSegundos(segundos);
+    }, 1000)
+}
 
+document.addEventListener('click', function(e){
+    const el = e.target;
+
+    if(el.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+    }
+
+    if(el.classList.contains('pausar')) {
+        clearInterval(timer);
+        relogio.classList.add('pausado');
+    }
+
+    if(el.classList.contains('zerar')){
+        relogio.classList.remove('pausado')
+        clearInterval(timer);
+        segundos = 0;
+        relogio.innerHTML = '00:00:00';
+    }
 });
+}
 
-pausar.addEventListener('click', function(event){
-    
-});
-
-zerar.addEventListener('click', function(event){
-    
-});
+relogio();
