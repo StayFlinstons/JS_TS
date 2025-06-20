a15bea101579e69065213c10e5c9ec245e91d55b
Richard <88545769+StayFlinstons@users.noreply.github.com>
2025-06-20 10:41:39 -0300

    huntshowdown

---
+++ JS/aula62/index.js
@@ -0,0 +1,17 @@
+//const nomes = new Array('Richard', 'Ismael', 'Joana');
+
+// const nomes = ['Richard', 'ismael', 'denys' ];
+// const novo = [...nomes];
+
+// novo.pop();
+// const removido = nomes.shift();
+// const removido1 = nomes.pop();
+
+// console.log(removido, nomes, removido1);
+
+// const nome = 'Richard Simões Cardoso';
+// const nomes = nome.split(' ');
+
+const nomes = ['Richard', 'ismael', 'denys' ];
+const nome = nomes.join(' ');
+console.log(nome);
---
+++ JS/aula63/index.js
@@ -0,0 +1,4 @@
+const nomes = ['Richard', 'ismael', 'denys', 'Preto', 'Branco'];
+
+const removidos = nomes.splice(3, 2, 'Joao')
+console.log(nomes, removidos);
